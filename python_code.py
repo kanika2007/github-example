bf39add38b8bf6f81e66b07c48ae69d0932737f0
kanika2007 <49320690+kanika2007@users.noreply.github.com>
2019-04-06 23:47:06 +0530

    committing final code

--- python_code.py
+++ python_code.py
@@ -1,23 +1,70 @@
-
-# coding: utf-8
-
-# this will be used for capstone project!-Kanika
-
-# In[ ]:
-
-
-import pandas as pd
-
-
-# In[ ]:
-
-
-import Numpy as np
-
-
-# In[1]:
-
-
-print("Hello Capstone Project Course!")
-
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "metadata": {
+    "collapsed": true
+   },
+   "source": [
+    "this will be used for capstone project!-Kanika"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import pandas as pd\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import Numpy as np"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Hello Capstone Project Course!\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"Hello Capstone Project Course!\")"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3.5",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.5.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 1
+}
 
